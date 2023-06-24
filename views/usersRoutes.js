927b354b51f9ef7126832e50d34dbccdf9bc9e12
Héctor Mateu <hmateu.ortola@gmail.com>
2023-06-24 13:52:00 +0200

    feat: Creates dentist and admin middleware

--- views/usersRoutes.js
+++ views/usersRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const usersGetAllControllers = require('../controllers/usersControllers/usersGetAllControllers');
+const isDentist = require('../middlewares/isDentist');
 const auth = require('../middlewares/verifyToken');
 
-router.get('/', auth, usersGetAllControllers.getAllUsers);
+router.get('/', auth, isDentist, usersGetAllControllers.getAllUsers);
 
 module.exports = router;
