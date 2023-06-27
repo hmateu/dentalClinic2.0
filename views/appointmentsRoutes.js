c265a48b418c9599433b5a7f6a1bb0eae27f6273
hmateu <hmateu.ortola@gmail.com>
2023-06-27 14:07:19 +0200

    feat: Creates the endpoint that shows all appointments for a patient

--- views/appointmentsRoutes.js
+++ views/appointmentsRoutes.js
@@ -1,6 +1,9 @@
+const appointmentGetOnePatientController = require('../controllers/appointmentsControllers/appointmentGetOnePatientController');
 const appointmentsGetAllController = require('../controllers/appointmentsControllers/appointmentsGetAll');
+const auth = require('../middlewares/verifyToken');
 const router = require('express').Router();
 
 router.get('/', appointmentsGetAllController.getAllAppointments);
+router.get('/patient', auth, appointmentGetOnePatientController.getOnePatientAppointments);
 
 module.exports = router;
