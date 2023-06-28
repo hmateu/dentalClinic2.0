3b899ce286fec0bbae3b4c847e5a5dc0cb638d00
hmateu <hmateu.ortola@gmail.com>
2023-06-28 02:14:43 +0200

    feat: Improves the query that displays a patient's appointments

--- controllers/appointmentsControllers/appointmentGetOnePatientController.js
+++ controllers/appointmentsControllers/appointmentGetOnePatientController.js
@@ -6,7 +6,7 @@ const appointmentGetOnePatientController = {};
 appointmentGetOnePatientController.getOnePatientAppointments = async (req,res) => {
     try {
         const patientId = req.userId;
-        const patientAppointments = await db.sequelize.query(`select a.id, a.date as date, a.price as price, a.assessment as assesment, ud.name as dentist, up.name as patient, s.name as service, p.name as payment from Appointments a left join Users ud on a.dentist = ud.id left join Users up on a.patient = up.id left join Services s on a.service = s.id left join Payments p on a.payment = p.id where a.patient = ${patientId}`, { type: sequelize.QueryTypes.SELECT });
+        const patientAppointments = await db.sequelize.query(`select a.id, a.date, a.price, a.assessment, ud.name as dentist, up.name as patient, s.name as service, p.name as payment from Appointments a left join Users ud on a.dentist = ud.id left join Users up on a.patient = up.id left join Services s on a.service = s.id left join Payments p on a.payment = p.id where a.patient = ${patientId}`, { type: sequelize.QueryTypes.SELECT });
         if(!patientAppointments){
             return res.json(
                 {
